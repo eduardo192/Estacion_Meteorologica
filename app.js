082b8908457755dca37021544222c9b252cf7e27
mgarcia43 <mgarcia43@ucol.mx>
2020-06-15 01:55:20 -0500

    Funcionalida a boton editar y borrar

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.database(); 
 console.log(db)
 
+obtenerDatos();
+
 //AGREGAR
 function crearNave(){
     let nomNave = document.getElementById('nomNave').value;
@@ -20,6 +22,8 @@ function crearNave(){
     let nomEstado = document.getElementById('nomEstado').value;
     let fecha = document.getElementById('fecha').value;
 
+    
+
     console.log(nomNave);
     const datos = {
         Cultivo: nomcul,
@@ -28,17 +32,17 @@ function crearNave(){
         Estado: nomEstado,
         Fecha: fecha
     };
-    db.ref(nomNave ).set(datos);
+    db.ref(nomNave).set(datos);
 
 }
 
-obtenerDatos();
+
 
 function obtenerDatos(){
     const tabla = document.getElementById('tabla');
-    let starCountRef = firebase.database().ref()
+    let starCountRef = db.ref()
     starCountRef.on('value', function(snapshot) {
-        
+        tabla.innerHTML = '';
         const d = snapshot.val();
         //console.log(d);
         for (const key in d) {
@@ -52,14 +56,48 @@ function obtenerDatos(){
                 <td>${d[key].Ciudad}</td>
                 <td>${d[key].Estado}</td>
                 <td>${d[key].Fecha}</td>
-                <td><button class="btn btn-danger">Eliminar</button></td>
-                <td><button class="btn btn-warning" >Editar</button></td>
+                <td><button class="btn btn-danger" onclick="borrar('${key}')">Borrar</button></td>
+                <td><button class="btn btn-warning" onclick="editar('${key}','${d[key].Cultivo}', '${d[key].Clima}', '${d[key].Ciudad}', '${d[key].Estado}', '${d[key].Fecha}')">Editar</button></td>
             </tr>`;
         }
     });
     
 }
 
-function borrarNaves(){
+function borrar(key) {console.log('borrar: ' + key);
+    db.ref(key).remove();
+}
+function editar(key, cultivo, clima, ciudad, estado, fecha) {
 
+
+    console.log('editar: ' + key);
+    const boton = document.getElementById('btnAgregar');
+    let nave = document.getElementById('nomNave');
+    nave.setAttribute('disabled', "true");
+    document.getElementById('nomNave').value = key;
+    document.getElementById('nomcul').value = cultivo;
+    document.getElementById('clima').value = clima;
+    document.getElementById('nomCiudad').value = ciudad;
+    document.getElementById('nomEstado').value = estado;
+    document.getElementById('fecha').value = fecha;
+
+    boton.innerHTML = 'Editar';
+
+    boton.onclick = function () {
+        let nomcul = document.getElementById('nomcul').value;
+        let clima = document.getElementById('clima').value;
+        let nomCiudad = document.getElementById('nomCiudad').value;
+        let nomEstado = document.getElementById('nomEstado').value;
+        let fecha = document.getElementById('fecha').value;
+        
+        const datos = {
+            Cultivo: nomcul,
+            Clima: clima,
+            Ciudad:nomCiudad,
+            Estado: nomEstado,
+            Fecha: fecha
+        };
+
+        db.ref(key).update(datos);
+    }
 }
