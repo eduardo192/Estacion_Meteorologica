a0994bff362d9f8db142a96bd536e01f969eeb32
mgarcia43 <mgarcia43@ucol.mx>
2020-06-14 22:13:51 -0500

    Cambio a real time

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-let nomNave = document.getElementById('nomNave').value;
 const firebaseConfig = {
     apiKey: "AIzaSyDWfxwXG0QhhnBpUGBugptz3AXnK1yem-w",
     authDomain: "estacion-86bac.firebaseapp.com",
-    projectId: "estacion-86bac",
+    databaseURL: "https://estacion-86bac.firebaseio.com",
+    storageBucket: "estacion-86bac.appspot.com"
 }
-function crearNave(){
 
+//INICIAR FIREBASE
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database(); 
+console.log(db)
+//AGREGAR
+function crearNave(){
+    let nomNave = document.getElementById('nomNave').value;
+    console.log(nomNave);
+    db.ref(nomNave + "/Sensores").set({
+        
+    });
 }
